0fa34a798f360aa2cb32e11f13c63046e2fd99ca
John Colagioia <john@colagioia.net>
2021-09-05 06:26:55 -0400

    Add Device Info Plus library to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
